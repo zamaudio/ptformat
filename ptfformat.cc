ed7afaa2f4d7dd688c311307642bcb871992ccb6
Damien Zammit <damien@zamaudio.com>
2015-07-29 22:35:26 +1000

    Remove debug printf
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- ptfformat.cc
+++ ptfformat.cc
@@ -488,7 +488,7 @@ PTFFormat::parse(void) {
 
 			regionspertrack = (uint8_t)(ptfunxored[k+13+lengthofname]);
 	
-			printf("regions/track=%d\n", regionspertrack);
+			//printf("regions/track=%d\n", regionspertrack);
 			char name[256] = {0};
 			for (j = 0; j < lengthofname; j++) {
 				name[j] = ptfunxored[j+k+13];
